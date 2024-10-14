e42b93cd37ee778296873dcfd73e5085f5bbbeb8
Wolfgang Schneider <wolfgang@easybyte.at>
2024-10-14 19:09:33 +0200

    .

--- FluentUI2/FluentUI2-CSS-BlazorWASM/CopyWWWROOT.bat
+++
@@ -1,4 +0,0 @@
-del wwwroot\css\fluentui2\*.* /s /q
-del wwwroot\js\*.* /s /q
-rem xcopy  /s ..\FluentUI2-CSS-RazorClassLibrary\wwwroot\css\fluentui2\*.* wwwroot\css\fluentui2
-rem xcopy  /s ..\FluentUI2-CSS-RazorClassLibrary\wwwroot\js\*.* wwwroot\js
